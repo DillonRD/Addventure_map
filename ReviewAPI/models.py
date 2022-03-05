26eb9b2531ce2d060084c8b47b1fcdd0db7aaa19
sayontaniray1 <sayontani.ray@my.utsa.edu>
2022-03-05 12:09:50 -0600

    Added DB table and mapping

--- ReviewAPI/models.py
+++ ReviewAPI/models.py
@@ -4,10 +4,13 @@ from django.db import models
 # Create your models here.
 
 class Review(models.Model):
-    review_id = models.IntegerField()
+    review_id = models.IntegerField(primary_key=True)
     user_id = models.IntegerField()
     location_id = models.IntegerField()
     review_image_id = models.IntegerField(null=True)
     date_post = models.DateField()
     reason = models.CharField(max_length=300)
     rating = models.DecimalField(max_digits=2, decimal_places=1)
+
+    class Meta:
+        db_table = "review"
--- ReviewAPI/urls.py
+++ ReviewAPI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FetchReviewsView, CreateReviewView
+from .views import FetchReviewsView, CreateReviewView, DeleteReviewView
 
 urlpatterns = [
     path('', FetchReviewsView.as_view()),
-    path('create', CreateReviewView.as_view())
+    path('create', CreateReviewView.as_view()),
+    path('delete', DeleteReviewView.as_view())
 ]
